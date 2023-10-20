ddb9b9aa3cda1436c105d1fc7063d1c4f22fed93
sebastian torres <sebas.developer@outlook.com>
2023-10-20 01:04:20 -0500

    Solucion retos #9

---
+++ Retos/Reto #9 - Special Pythagorean Triplet/Program.cs
@@ -0,0 +1,12 @@
+﻿for (int a = 1; a < 333; a++)
+{
+    for (int b = a + 1; b < 500; b++)
+    {
+        int c = 1000 - a - b;
+        if (a*a + b*b == c*c)
+        {
+            Console.WriteLine(a*b*c);
+            break;
+        }
+    }
+}
