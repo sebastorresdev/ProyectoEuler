c31041d119c83e46a889c1a6292d3b67d3706044
sebastian torres <sebas.developer@outlook.com>
2023-10-22 01:23:51 -0500

    Solucion retos #10

---
+++ Retos/Reto #10 - Summation of Primes/Program.cs
@@ -0,0 +1,31 @@
+﻿// suma de los primos debajo de 2 millones
+
+using System.Diagnostics;
+
+Stopwatch stopwatch = new();
+stopwatch.Start();
+
+// Uso del algoritmode criba de Eratóstenes
+
+int count = 2000000;
+bool[] isPrime = new bool[count];
+long sum = 0;
+
+for (int i = 2; i < count; i++)
+{
+    if (!isPrime[i])
+    {
+        sum += i;
+        for (int j = i * 2; j < count; j += i)
+        {
+            isPrime[j] = true;
+        }
+    }
+}
+
+Console.WriteLine(sum);
+
+stopwatch.Stop();
+
+long seconds = stopwatch.ElapsedMilliseconds;
+Console.WriteLine("Se demora {0} Millisegundos", seconds);
